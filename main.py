21088a9dcc09d423335f863dfe209bce2a18bd6b
Rex <jemmyperez28@gmail.com>
2025-06-17 15:11:58 -0500

    Fix2

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return jsonify({"Choo Choo": "Welcome to your Flask app 🚅"})
+    return jsonify({"Choo Choo": " Welcome to your Flask app 🚅"})
 
 @app.route('/form')
 def mostrar_formulario():
